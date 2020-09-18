6bff5a9cb5336d645898b7f024f63a943115598a
redwoodcodeacademy <info@redwoodcodeacademy.com>
2020-09-18 10:37:57 -0700

    Updates

---
+++ precache-manifest.644f125fa7ee4443627ee80c59b9ddc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37b47d7137d46acf178f76808ca77ec",
+    "url": "/hacker-news-clone/index.html"
+  },
+  {
+    "revision": "0a95d1ff14f1c18ffcf8",
+    "url": "/hacker-news-clone/static/js/2.01a2b292.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/hacker-news-clone/static/js/2.01a2b292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f942ade94726f787e9",
+    "url": "/hacker-news-clone/static/js/main.097119af.chunk.js"
+  },
+  {
+    "revision": "c9a3baf2b0bc3fd6620c",
+    "url": "/hacker-news-clone/static/js/runtime-main.033094b6.js"
+  }
+]);
